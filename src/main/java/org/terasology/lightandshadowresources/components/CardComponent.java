4997af4e482dbe5dfe687fbf7eee060338afc4c7
Michael Pollind <mpollind@gmail.com>
2021-08-25 12:54:28 -0700

    feature(ecs-gestalt): Migrate Components to gestalt's Components (#71)
    ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/lightandshadowresources/components/CardComponent.java
+++ src/main/java/org/terasology/lightandshadowresources/components/CardComponent.java
@@ -1,17 +1,23 @@
 // Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
-
 package org.terasology.lightandshadowresources.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.entitySystem.prefab.Prefab;
 import org.terasology.engine.world.block.family.BlockFamily;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * This is the component class for playing cards, which are constructed of a top and a bottom block.
  */
-public class CardComponent implements Component {
+public class CardComponent implements Component<CardComponent> {
     public BlockFamily topBlockFamily;
     public BlockFamily bottomBlockFamily;
     public Prefab cardBlockPrefab;
+
+    @Override
+    public void copyFrom(CardComponent other) {
+        this.topBlockFamily = other.topBlockFamily;
+        this.bottomBlockFamily = other.bottomBlockFamily;
+        this.cardBlockPrefab = other.cardBlockPrefab;
+    }
 }
